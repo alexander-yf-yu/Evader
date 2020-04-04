815f29afd6efff01dba2eccc4c8abff6654ac8f1
alexyu <alexander.yf.yu@gmail.com>
2020-04-04 16:59:34 -0700

    had an idea

--- policy.py
+++ policy.py
@@ -4,40 +4,37 @@ from __future__ import print_function
 
 from env import EvaderEnv as Env
 import tensorflow as tf
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from tf_agents.agents.dqn import dqn_agent
+from tf_agents.agents.reinforce import reinforce_agent
 from tf_agents.drivers import dynamic_step_driver
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 from tf_agents.eval import metric_utils
 from tf_agents.metrics import tf_metrics
-from tf_agents.networks import q_network
-from tf_agents.policies import random_tf_policy
+from tf_agents.networks import actor_distribution_network
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
-
 
 # suppress warning about GPU usage
 from os import environ
 environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Hyper parameters
-num_iterations = 20000 # @param {type:"integer"}
+num_iterations = 250        # @param {type:"integer"}
+collect_episodes_per_iteration = 2  # @param {type:"integer"}
+replay_buffer_capacity = 2000   # @param {type:"integer"}
 
-initial_collect_steps = 1000  # @param {type:"integer"}
-collect_steps_per_iteration = 1  # @param {type:"integer"}
-replay_buffer_max_length = 100000  # @param {type:"integer"}
+fc_layer_params = (100,)
 
-batch_size = 64  # @param {type:"integer"}
-learning_rate = 1e-3  # @param {type:"number"}
-log_interval = 200  # @param {type:"integer"}
-
-num_eval_episodes = 100   # @param {type:"integer"}
-eval_interval = 1000  # @param {type:"integer"}
+learning_rate = 1e-3    # @param {type:"number"}
+log_interval = 25   # @param {type:"integer"}
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 50  # @param {type:"integer"}
 
 tf.compat.v1.enable_v2_behavior()
 
